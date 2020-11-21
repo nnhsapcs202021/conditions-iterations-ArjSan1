c3ac2b4f306254d8a94f287f0200b0a832e16030
ArjSan1 <66225751+ArjSan1@users.noreply.github.com>
2020-11-21 11:41:49 -0600

    Finished comparing Strings notes

--- Conditions.java
+++ Conditions.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /**
  * Examples and notes on conditions (Chapter 5)
@@ -87,6 +88,73 @@ public class Conditions
     }
     // dont have to use an if statement
     //just reuturn the (Math.abs(num1-num2) < EPSILON
-    
+  
 }
+/*
+ * == returns true if the strings contain the same value
+ *  For variables of a class type, including string, this means they contain the same reference.
+ *  They refer to the same object in memory, not that the two strings have the same sequence
+ *  of characters. 
+ */
+  public static void stringExample()
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter two words: ");
+        String str1 = s.next();
+        String str2 = s.next();
+        if (str1 == str2)
+        {
+            System.out.println("Strings refs are equal");
+            
+        }
+        else
+        {
+            System.out.println("strings refs are not equal");
+        }
+        /*
+         * The equals method returns tru if the two objects referenced by the varuibles are "equal"
+         * What equals means is defined by the class. For strings it means that the two objects have the same
+         *  sequence of characters
+         */
+        
+        if (str1.equals(str2))
+        {
+            System.out.println("Strings are equal");
+            
+        }
+        else
+        {
+            System.out.println("strings are not equal");
+        }
+        /*
+         * We will determine which string comes first lexicographically using the compareTo
+         * method of the string class. 
+         * 
+         * compareTo returns an int:
+         *      0: if the strings are equal (same sequence of characters)
+         *      < 0: if str1 < str2 lexicographically
+         *      > 0: if str1 > str2 lexicographically
+         */
+        int result = str1.compareTo(str2);
+        String firstString = null;
+        
+        if(result < 0)
+        {
+            firstString = str1;
+        }
+        else if (result > 0)
+        {
+            firstString = str2;
+        }
+        if (firstString != null)
+        {
+        System.out.println("The first string is: " + firstString);
+        System.out.println("Its length is: " + firstString.length());
+    }
+    else
+    {
+                   System.out.println("Strings are equal");
+ 
+    }
+    }
 }
