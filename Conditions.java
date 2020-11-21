18754933e281998c73aee3b8a42ea29703c2466e
ArjSan1 <66225751+ArjSan1@users.noreply.github.com>
2020-11-21 10:05:20 -0600

    Finished if, else if, else , and comparing integers notes

--- Conditions.java
+++ Conditions.java
@@ -2,10 +2,71 @@
 /**
  * Examples and notes on conditions (Chapter 5)
  *
- * @author gcschmit
- * @version 16nov2020
+ * @author ArjSan1
+ * @version 20nov2020
  */
 public class Conditions
 {
-    
+    public static void ifExample()
+    {
+        // model a coin flip
+        int coinFlip = (int)(Math.random()*2);
+        
+        
+        /* Executed if the conditional expression is true
+         * Conditional are true or false
+         * Conditionals must go inside paranthesis
+         * Statements are grouped by curly braces
+         * No colon after statement
+         * 
+         */
+        if (coinFlip ==1)
+        {
+            System.out.println("Coin is heads!");
+        }
+        
+        /*
+         * if-else statement
+         *  The else block executes if the condition is false
+         */
+        if (coinFlip ==1)
+        {
+            System.out.println("Coin is heads!");
+        }
+        else 
+        {
+            System.out.println("Coin is tails!");
+        }
+        
+        
+        //model the roll of a 4 sided die [1-4]
+        int dieRoll = (int)(Math.random()*4) +1;
+        
+        /*
+         * if-else if-else statement
+         *  In Java else if is used instead of elif in Python
+         */
+        if (dieRoll ==1)
+        {
+            System.out.println("rolled a one");
+        }
+        else if (dieRoll ==2)
+        {
+            System.out.println("rolled a two");
+            
+        }
+        else if (dieRoll ==3)
+        {
+            System.out.println("rolled a three");
+            
+        }
+        else 
+        {
+                        System.out.println("rolled a four");
+
+        }
+        
+        if (coinFlip == 0)
+            System.out.println("Coin is tails!");
+    }
 }
